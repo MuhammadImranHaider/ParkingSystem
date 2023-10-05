1cd6f4a9d13d6357d23af1f4b31707460c97ee5e
Imran Haider <imranhaider.se@gmail.com>
2023-10-06 04:20:23 +0500

    Add project files.

---
+++ ParkingSystem/ParkingSystem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingSystemApp
+{
+	public class ParkingSystem
+	{
+		private int[] availableSlots = default;
+		public ParkingSystem(int big, int medium, int small)
+		{
+			availableSlots = new int[] { big, medium, small };
+		}
+
+		public bool AddCar(int carType)
+		{
+			const int minCarType = 1;
+			const int maxCarType = 3;
+			if (carType < minCarType || carType > maxCarType) {
+				return false;
+			}
+			int index = carType - 1; // let say if carType = 3; which is small. Small stored at index 2. now we will check if there is any space available.
+			if (availableSlots[index] > 0) 
+			{
+				availableSlots[index]--; // one more slot occupied so will minus it.
+				return true;
+			}
+
+			return false; // No space available for this size.
+		}
+	}
+
+	/**
+	 * Your ParkingSystem object will be instantiated and called as such:
+	 * ParkingSystem obj = new ParkingSystem(big, medium, small);
+	 * bool param_1 = obj.AddCar(carType);
+	 */
+}
---
+++ ParkingSystemTests/ParkingSystemTests.cs
@@ -0,0 +1,60 @@
+﻿using ParkingSystemApp;
+using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ParkingSystemTests
+{
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class ParkingSystemTests
+	{
+		[TestMethod]
+		public void AddCar_BigCarWithAvailableSpace_ReturnsTrue()
+		{
+			ParkingSystem parkingSystem = new ParkingSystem(3, 5, 7);
+			bool result = parkingSystem.AddCar(1); 
+			Assert.IsTrue(result);
+		}
+
+		[TestMethod]
+		public void AddCar_MediumCarWithAvailableSpace_ReturnsTrue()
+		{
+			ParkingSystem parkingSystem = new ParkingSystem(3, 5, 7);
+			bool result = parkingSystem.AddCar(2); 
+			Assert.IsTrue(result);
+		}
+
+		[TestMethod]
+		public void AddCar_SmallCarWithAvailableSpace_ReturnsTrue()
+		{
+			ParkingSystem parkingSystem = new ParkingSystem(3, 5, 7);
+			bool result = parkingSystem.AddCar(3); 
+			Assert.IsTrue(result);
+		}
+
+		[TestMethod]
+		public void AddCar_BigCarWithNoSpace_ReturnsFalse()
+		{
+			ParkingSystem parkingSystem = new ParkingSystem(0, 5, 7);
+			bool result = parkingSystem.AddCar(1); 
+			Assert.IsFalse(result);
+		}
+
+		[TestMethod]
+		public void AddCar_InvalidCarType_ReturnsFalse()
+		{
+			ParkingSystem parkingSystem = new ParkingSystem(3, 5, 7);
+			bool result = parkingSystem.AddCar(4);
+			Assert.IsFalse(result);
+		}
+	}
+
+}
+
+
